051a06c70f84c29753611a6890c49b8023e726e3
Arnold Paye <arnoldpayet@gmail.com>
2022-01-25 07:28:30 -0400

    Add CounterButton component

---
+++ src/CounterButton.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const CounterButton = () => {
+  const [numberOfClicks, setNumberOfClick] = useState(0);
+  const [incrementBy, setIncrementBy] = useState(1);
+
+  return (
+    <>
+      <p>You have clicked the button {numberOfClicks} times.</p>
+      <label>
+        Increment By:
+        <input value={incrementBy} onChange={e => setIncrementBy(Number(e.target.value))} type="number" />
+      </label>
+      <button onClick={() => setNumberOfClick(numberOfClicks + incrementBy)}>Click</button>
+    </>
+  )
+};
